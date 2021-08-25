a977a3e4edf7464b8c34824506e870f6d1f12fd0
Nazim Malik <69416004+Nazimmalik421@users.noreply.github.com>
2021-08-25 14:06:57 +0530

    Update Footer.js

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -4,16 +4,8 @@ const Footer = () => {
 
     return (
         <footer className='footer'>
-            {/* <ul class="nav">
-                <li class="nav__item"><a href="#" class="nav__link">Find your dream home</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">Request propsal</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">Download home planner</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">Contact us</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">Submit your property</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">Come work with us</a></li>
-            </ul> */}
+
             <p className="copyright">
-                {/* Built with ♥ by Mohd Nazim */}
                 &copy; Copyright 2021 by New Concept Interiors
             </p>
             <p className="intro">
